000d5b9383188d665ad44fca86b5c4c5223b51a6
Louie Hext <57326145+LouieHext@users.noreply.github.com>
2021-02-27 22:03:44 +0000

    Add files via upload

--- nois.py
+++ nois.py
@@ -15,6 +15,9 @@ Created on Sat Aug 22 19:19:08 2020
 import numpy as np
 import random as rand
 import matplotlib.pyplot as plt
+import time as time
+import matplotlib
+#matplotlib.use('GTK3Agg') 
 
 
 class Ball():
@@ -25,7 +28,7 @@ class Ball():
         self.n=n
         self.size=size
         self.max_vel=max_vel
-        self.pos_x=np.random.uniform(0,size,(n))
+        self.pos_x=np.random.uniform(1,size,(n))
         self.pos_y=np.random.uniform(0,size,(n))
         self.vel_x=np.zeros(n)
         self.vel_y=np.zeros(n)
@@ -86,10 +89,11 @@ class Ball():
             fx,fy=self.get_forces(vector_x,vector_y)
             self.apply_force(fx,fy)
             self.update()
-            self.edge_check()
-            self.speed_check()
+            
             self.x_hist=np.vstack([self.x_hist,self.pos_x])
             self.y_hist=np.vstack([self.y_hist,self.pos_y])
+            self.edge_check()
+            self.speed_check()
             count=count+1
         return self.x_hist,self.y_hist
             
@@ -140,8 +144,15 @@ def perlin(n,m):
     """
     n>m (ints)
     """
+    
+    
     scale=int(n/m)+1
+    
     vectors=np.random.uniform(-1,1,(scale,scale,2)) #grid of random vectors
+    vectors[-1,:]=vectors[0,:]
+    vectors[-2,:]=vectors[1,:]
+    vectors[:,-1]=vectors[:,0]
+    vectors[:,-2]=vectors[:,1]
     data=np.ones((n,n))
     global a,b,c,d
     a=inner_grid_setup(0,1,0,-1,m)  #sets up inner grids
@@ -154,71 +165,278 @@ def perlin(n,m):
             heights=get_inner_prevals(corners, m)
             data[i*m:(i+1)*m,j*m:(j+1)*m]=heights
     
+    #plot(n,data)
     return data
 
 def plot(n,data,name="test"):
-    n=2**n
-    plt.figure(2,figsize=(19.20*4,10.80*4))
+    #n=2**n
+    plt.figure()
+    fig = plt.gcf()
+    fig.set_size_inches(19.2,10.8)
     plt.clf()
     plt.axis('off')
-    fig=plt.figure(1)
+    
     ax = fig.add_subplot(1,1,1)
+    plt.axis('off')
     x=np.linspace(0,n,n)
     y=np.linspace(0,n,n)
     plot=ax.pcolormesh(x, y, data,cmap="Greys")
-    plt.savefig("%s"%(name), dpi=300)
+    #X, Y = np.meshgrid(x, y)
+    #plot=ax.contourf(X, Y, data, 15, cmap=plt.cm.bone,alpha=0.02)
+    # plot = ax.contourf(x, y, data, hatches=['-', '/', '\\', '//'],
+    #              cmap='gray', extend='both', alpha=0.5)
 
+    #plt.savefig("%s"%(name), dpi=300)
+    
 
-
-def fractal(n,k):
+def fractal(n,m,k):
      """
      n>k (ints)
      super imposes different frequency perlin noise
      
      """
      maximum=2**n
+     
      data=np.zeros((maximum,maximum))
      for i in range(k):
-         temp=perlin(maximum,2**(n-i))*(2**(-(1+i)))
-         data=data+temp
+         temp=perlin(maximum,2**(m-i))*(2**(-(m+i)))
+         print(np.shape(temp))
+         data=data+temp/k
          #plot(n,data/k,i)
-     data=data/k   
-     #data=1-np.abs(data)
-     #plot(n,data)
+     data=data
+     # data=data/np.amax(data)
+     # data=1-np.abs(data)-data
+     plot(maximum,data)
      return data
  
+def curl(n,m,k,balls,updates,max_vel=5,wildness=25,x_scale=50,y_scale=100):
+    s=time.time()
+    pot=fractal(n,m,k)
+    #pot=perlin(2**n,2**k)
+    size=len(pot[0])
+    vector_x,vector_y=vector_field(pot,wildness,x_scale,y_scale)
+    x=np.gradient(vector_y,axis=1)
+    #x=x+np.amax(x)/10
+    y=-np.gradient(vector_x,axis=0)
+    #y=y+np.amax(y)/10
+    x,y=dipoles(x, y, 8, 100000)
+    balls=Ball(size=size,n=balls,max_vel=max_vel)
+    x_hist,y_hist=balls.drive(x, y, updates)
     
-def plot_flow(x_hist,y_hist):
-    x_hist=x_hist.transpose()   
-    y_hist=y_hist.transpose()
+    #plot_flow(x_hist, y_hist)
+    non_overlap_plot(x_hist, y_hist)
+    
+    
+    print(time.time()-s)
+
+    
+def animate_flow(x_hist,y_hist): 
     plt.style.use("default")
-    #plt.style.use('dark_background')
+   
     plt.axis('off')
-    plt.figure(1,figsize=(19.20,10.80))
+   
     
-    c_list=["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]
+    #c_list=["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]
     #c_list=["#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+    #c_list=["#007D32","#6BFFA6","#00FC65","#5B8069","#00CC50","#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
     #c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
-    #c_list=["black"]
-    s_list=[2,2,2,2,5,2,10,15]
-    #s_list=[10]
+    c_list=["black"]
+    s_list=[5,5,5,5,10,5,2,15,10]
+    #s_list=[5]
     
     for i in range(len(x_hist)):
+         plt.figure(1,figsize=(19.20,10.80)) 
+
+         plt.style.use("default")
+         plt.axis('off')
+        # color=rand.choice(c_list)
+        # s=rand.choice(s_list)
+         count=0
+         color=rand.choice(c_list)
+         
+         while count<len(x_hist[i]):
+             s=rand.choice(s_list)
+             step=rand.randint(count, len(x_hist[i]))
+             temp_x=x_hist[i][count:count+step]
+             temp_y=y_hist[i][count:count+step]
+             plt.scatter(temp_x,temp_y,s=s,alpha=0.3,color=color,edgecolors="black",markr="_")
+             count=count+step
+             
+         plt.savefig("%s"%(i), dpi=600)   
+
+        
+def non_overlap_plot(x,y):
+    x=x.T
+    y=y.T
+    
+    #c_list=["white","white","white","#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+    #c_list=["black","white","#FCED0D","#FFF200","white","black","black","black","white","white"]
+    #c_list=["#007D32","#6BFFA6","#00FC65","#5B8069","#00CC50","#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+    #c_list=["#38C8F5","#54D7FF","#1686A8","#A86D32","#F58C76","#A86759","#EBDC3B","#ED721A"]
+    #c_list=["#1972D1","#D13224","#0ED0D1","#D19124","#17D16A","#5AD1B3","#D18B28"]
+    #c_list=["#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+    #c_list=["#7A2418"]
+    c_list=["grey","black","white"]
+    
+    #c_list=["white"]
+    #s_list=[30]
+    fig=plt.figure()
+    fig.set_size_inches(19.2,10.8)
+    plt.tick_params(top=False, bottom=False, left=False, right=False,
+                labelleft=False, labelbottom=False)
+    
+    ax = plt.gca()
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white') 
+    ax.spines['right'].set_color('white')
+    ax.spines['left'].set_color('white')
+    
+    ax.set_aspect(1)
+    
+    fig.canvas.draw()
+    
+    x=np.reshape(x,(np.shape(x)[0]*np.shape(x)[1]))
+    y=np.reshape(y,(np.shape(y)[0]*np.shape(y)[1]))
+    size=(max(x),max(y))
+    size=max(size)
+    print(size)
+    #s_list=size/np.array([100,50,200,400,150,170])
+    s_list=size/np.array([190,200,200,400,150,170,100,85,125,1000,80,90,75,])
+    #s_list=size/np.array([100,110,90,85,105,115])*1.25
+    #s_list=size/np.array([100])
+    count=0
+    
+    
+    a=rand.randint(0,len(x)/5)
+    b=(len(x)/5-a)*rand.random()
+    a=[a,a+5000,a+10000,a+15000,a+20000,a+25000]
+    alpha=[0.15,0.2,0.3,0.4]
+    
+    while count<len(x):
+    #while count<4:    
         color=rand.choice(c_list)
-        s=rand.choice(s_list)
-        plt.scatter(x_hist[i],y_hist[i],s=s,alpha=0.11,color=color)
+        t=rand.choice(s_list)
+        ty=t
+        step=int(rand.randint(count, len(x))/20)
+        # temp_x=x[a[count]:a[count]+int(b)]
+        # temp_y=y[a[count]:a[count]+int(b)]
+        
+        temp_x=x[count:count+step]
+        temp_y=y[count:count+step]
+        # temp_x=x
+        # temp_y=y
+        
+        temp_x=(np.around(temp_x/t))*t
+        
+        temp_y=(np.around(temp_y/ty))*ty
+
+        points=[(temp_x[i],temp_y[i]) for i in range(len(temp_x))]
+        points=list(set(points))
+       
+        temp_x=np.array(points)[:,0]
+        temp_y=np.array(points)[:,1]
+                                             
+
+        s = ((ax.get_window_extent().width*(t/(0.5*np.pi) ) / (size) * 72./fig.dpi) ** 2) #r=0.5 data units
+       
+        ax.scatter(temp_x,temp_y,s=s,alpha=rand.choice(alpha)/2,color=color,)
+        #ax.patch.set_facecolor('#ED723B')
+        #ax.patch.set_facecolor('#D15028')
+        ax.patch.set_alpha(0.5)
+        #count=count+1
+        count=count+step
+    
+
+def plot_flow(x_hist,y_hist):
+    plt.figure()
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_size_inches(19.2,10.8)
+    plt.axis('off')
+    
+    x_hist=x_hist.transpose()   
+    y_hist=y_hist.transpose()
+
+    plt.style.use("default")
+    #plt.style.use('dark_background')
+ 
+   
+    
+    #c_list=["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]
+    #c_list=["#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+    # c_list=["#007D32","#6BFFA6","#00FC65","#5B8069","#00CC50","#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+    c_list=["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]
+    ##c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+    #c_list=["black","white","#FCED0D","#FFF200","white","black","black","black","white","white"]
+    #c_list=["#7A2418"]
+    s_list=[1,1,1,1,5,5,5,5,10,5,2,15,25,30,5,10]
+    #s_list=[15]
+    
+    for i in range(len(x_hist)):
+        # color=rand.choice(c_list)
+        # s=rand.choice(s_list)
+        count=0
+        while count<len(x_hist[i]):
+            color=rand.choice(c_list)
+            s=rand.choice(s_list)
+            step=rand.randint(count, len(x_hist[i]))
+            temp_x=x_hist[i][count:count+step]
+            temp_y=y_hist[i][count:count+step]
+            plt.scatter(temp_x,temp_y,s=s*2.5,alpha=0.2,color=color,)
+            count=count+step
+        
+       #plt.scatter(x_hist[i],y_hist[i],s=s,alpha=0.11,color=color)
     plt.show()    
-    #plt.savefig("waves6",dpi=600)
-    #plt.savefig('myimage.svg', format='svg', dpi=1200)
+    name=str(rand.random())
+    plt.savefig("redddfd",dpi=600)
+    #plt.savefig('urban.svg', format='svg', dpi=1200)
+
+
+
+def dipoles(vector_x,vector_y,num,strength):
+    size=np.shape(vector_x)[0]
+    x=np.linspace(0,size,size)
+    y=np.linspace(0,size,size)
+    xx,yy=np.meshgrid(x,y)
+    Ex=np.zeros((size,size))
+    Ey=np.zeros((size,size))
+    for i in range(num):
+        px,py=strength*np.random.uniform(-5,5,size=(2,1))
+        pos_x,pos_y=np.random.uniform(0,size,size=(2,1))
+        rx=xx-pos_x
+        ry=yy-pos_y
+        r=(rx*rx+ry*ry)**0.5
+        rx_norm=rx/r
+        
+        ry_norm=ry/r
+
+        dot=rx_norm*px+ry_norm*py
+        ex=(3*dot*rx_norm-px)/r**2
+        ey=(3*dot*ry_norm-py)/r**2
+        print(ex)
+        Ex=Ex+ex
+        Ey=Ey+ey
+        
+        # plt.figure()
+    # ax = plt.gca()
+    # plot=ax.pcolormesh(x, y, grid,cmap="Greys")
+        
+    plot_vectors(Ex,Ey,size)
+    return vector_x+Ex,vector_y+Ey    
          
 def plot_vectors(vector_x,vector_y,size):
+    plt.figure()
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_size_inches(19.2,10.8)
+    plt.axis('off')
     plt.style.use('default')
     plt.axis('off')
-    plt.figure(3,figsize=(19.20,10.80))
+    
     x=np.linspace(0,size,size+1)
     y=np.linspace(0,size,size+1)
     xx,yy=np.meshgrid(x,y)
-    plt.quiver( xx,yy,vector_x,vector_y,color="black")
+    plt.quiver( xx,yy,vector_x,vector_y,color="black",scale=50)
     plt.show()    
         
 def rotate(xs,ys,angles):
@@ -228,27 +446,115 @@ def rotate(xs,ys,angles):
 
 def vector_field(data,wildness=25,x_scale=50,y_scale=100):
     size=len(data[0])
-    vector_x=np.ones((size,size))
-    vector_y=np.zeros((size,size))
+    vector_y=np.ones((size,size))
+    vector_x=np.zeros((size,size))
     angles=2*np.pi*data*wildness
     vector_x,vector_y=rotate(vector_x,vector_y,angles)
     vector_x=vector_x*x_scale
     vector_y=vector_y*y_scale
-    plot_vectors(vector_x,vector_y,size)
+    #plot_vectors(vector_x,vector_y,size)
     
     return vector_x,vector_y
 
-def run(n,k,balls,updates,max_vel=5,wildness=25,x_scale=50,y_scale=100):
+def plot_line(data,y):
+    
+    plt.figure()
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_size_inches(19.2,10.8)
+    plt.axis('off')
+    plt.style.use('dark_background')
+    for i in range(len(data)):
+        #c_list=["#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+        #c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+        c_list=["#007D32","#6BFFA6","#00FC65","#5B8069","#00CC50","#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+        #c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+        color=rand.choice(c_list)
+        plt.plot(data[i],y,color=color,alpha=0.25)
+
+def plot_circle(data,theta):
+    
+    plt.figure()
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_size_inches(19.2,10.8)
+    plt.axis('off')
+    plt.style.use('dark_background')
+    for i in range(len(data)):
+        x=data[i]*np.cos(theta)
+        y=data[i]*np.sin(theta)
+        #c_list=["#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+        #c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+        #c_list=["#010408","#007D32","#6BFFA6","#00FC65","#5B8069","#00CC50","#04577A","#53C8FB","#07B1FA","#28627A","#068CC7","white"]
+        #c_list=["#7A2418","#FB8C7D","#FA4932","#7A443D","#C23827"]
+        c_list=["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]
+        color=rand.choice(c_list)
+        
+        plt.plot(x,y,color=color,alpha=0.12)
+
+def run(n,m,k,balls,updates,max_vel=5,wildness=25,x_scale=500,y_scale=1000):
     """ n>k (ints)
     
     """
-    data=fractal(n,k)
+    s=time.time()
+    data=fractal(n,m,k)
+    
     size=len(data[0])
     vector_x,vector_y=vector_field(data,wildness,x_scale,y_scale)
+    #vector_x,vector_y=dipoles(vector_x, vector_y, 1, 100000)
+    
+    
     balls=Ball(size=size,n=balls,max_vel=max_vel)
     x_hist,y_hist=balls.drive(vector_x, vector_y, updates)
-    plot_flow(x_hist, y_hist)
     
+    #plot_flow(x_hist, y_hist)
+    non_overlap_plot(x_hist, y_hist)
+    #plot(n,data)
+    
+    print(time.time()-s)
+    #animate_flow(x_hist, y_hist)
+    
+def run_lines(n,k):
+    var=fractal(n,k)*9000
+    #var=perlin(2**n,2**k)*80
+    size=np.shape(var)[0]
+    x=np.linspace(0,size,size)
+    y=np.linspace(0,size,size)
+    X,Y=np.meshgrid(x,y)
+    data=var+X
+    columns=[]
+    for i in range(np.shape(data)[0]):
+        temp=data[:,i]
+        columns.append(temp)
+    plot_line(columns,y)    
 
-run(7,6,100,50)
+def run_circle(n,m,k):    
+    #var=perlin(2**n,2**k)*800
+    var=fractal(n,m,k)*40000
+    size=np.shape(var)[0]
+    r=np.linspace(size/9,size,size)
+    # b=np.linspace(size/4,size/2,int(size/2))
+    # r=np.vstack([r,b])
+    theta=np.linspace(0,1.998*np.pi,size)
+    R,Theta=np.meshgrid(r,theta)
+    data=var+R
+    columns=[]
+    for i in range(np.shape(data)[0]):
+        temp=data[:,i]
+        # temp2=temp[::-1]
+        # temp=temp+temp2
+        columns.append(temp)
+    flip=-np.array(columns)*(-1)
+    plot_circle(columns,theta)
+    # theta2=np.linspace(0,-np.pi,size)
+    # plot_circle(columns,theta2)    
+#
+run(7,5,2,3000,1000,max_vel=5,wildness=5,x_scale=9,y_scale=9)
+#curl(7,6,3,3000,1000,max_vel=5,wildness=25,x_scale=50,y_scale=50)
+#curl(9,3,5000,1000,max_vel=25,wildness=15,x_scale=5,y_scale=10)
+#perlin(2**9,2**3)
+#fractal(11,7,4)
+#run_lines(10,9)
+#run_circle(6,3,2)
+
 
